590be6c486d12723ca06aee1a74a2fd1d41e5a85
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-05 15:50:02 -0700

    Blog added and working

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog model
+class Blog(models.Model):
+    title = models.CharField(max_length = 30)
+    pub_date = models.DateTimeField()
+    body = models.CharField(max_length = 500)
+    image = models.ImageField(upload_to = 'images/')
