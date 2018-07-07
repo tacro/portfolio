3339d69cdc1bc10709c28c22fa6ff149f5ba0c03
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-08 07:57:11 +0900

    enable to show detail of blog post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
 def index(request):
     posts = Blog.objects
     return render(request, 'blog/index.html', {'posts':posts})
+
+def detail(request, blog_id):
+    post = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'blog/detail.html', {'post':post})
