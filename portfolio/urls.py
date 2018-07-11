f964cbdba5659828ed3bf384180958d4b321bfba
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-11 15:17:30 +0900

    added about page and SNS link

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  jobs.views.home, name = 'home'),
     path('blog/', include('blog.urls')),
+    path('about/', jobs.views.about, name='about'),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
